56bffd9a91d63f6f5b99f6a8c8b8640e490febc6
Joao Vitor <jvads2005@gmail.com>
2024-06-12 15:49:35 -0300

    chore:  delete functionality to BlogPostListCreate

--- app/api/views.py
+++ app/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import generics, status
 from .models import BlogPost
 from .serializers import BlogPostSerializer
 
@@ -7,6 +8,10 @@ from .serializers import BlogPostSerializer
 class BlogPostListCreate(generics.ListCreateAPIView):
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
+
+    def delete(self, request, *args, **kwargs):
+        BlogPost.objects.all().delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 class BlogPostRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = BlogPost.objects.all()
